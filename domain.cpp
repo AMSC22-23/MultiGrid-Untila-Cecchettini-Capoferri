2a5ea4c93446fa475d57441f238580ae0507c225
Lucacapoferri <luca2.capoferri@mail.polimi.it>
2023-11-21 10:11:01 +0000

    apposto anche per negativi

--- domain.cpp
+++ domain.cpp
@@ -10,7 +10,7 @@ for this case we describe the domain as 4 limits...for the most general case wou
 //definition of my_tuple where i save my coordinates
 typedef  std::vector < std::tuple<double, double>> my_tuple;
 /*paasserò un puntatore ad un vettore inizializzato vuoto di tuple*/
-void Domain_qnt(my_tuple* list, int N, double B_left, double B_right, double B_top, double B_down) {
+void Domain_qnt(my_tuple &list, int N, double B_left, double B_right, double B_top, double B_down) {
 
     double h;
 
@@ -18,22 +18,27 @@ void Domain_qnt(my_tuple* list, int N, double B_left, double B_right, double B_t
     h = std::abs((B_right - B_left)/(std::round(sqrt(N))-1));
 
     //discretizzo il dominio per riga
-    
-    for(double i=B_top; i>=B_down; i -= h){
-        for(double j= B_right; j<= B_left; j += h){
-            list->push_back(std::make_tuple(i,j));
+    if(list.size()!=0)
+        return;
+    for(double j=B_top; j>=B_down; j -= h){
+        for(double i= B_left; i<= B_right; i += h){
+            list.push_back(std::make_tuple(i,j));
         }
     }
 }
 
 
-int main(){
+int main(int argc, char** argv){
 
 my_tuple list;
-Domain_qnt(&list, 576,500,450,200,150);
+Domain_qnt(list,97,-10.0,40.0,20.0,-30.0);
+
+for(int i=0;i<argc; i++){
+    std::cout<<argv[i]<<std::endl;
+}
 
 //stampo i tuple cioe le quantizzazioni del mio dominio
-for(int k=0; k < list.size() ; k++){
+for(size_t k=0; k < list.size() ; k++){
         std::cout<< "(" << std::get<0>(list[k]) << " - " << std::get<1>(list[k]) << ")" <<std::endl;
 }
 
