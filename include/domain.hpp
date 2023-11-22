5362dda3b3f9bff8cb546d60485b13b7e65d7037
Stefano Cecchettini <stefano.cecchettini1@gmail.com>
2023-11-22 19:22:03 +0100

    dev

--- include/domain.hpp
+++ include/domain.hpp
@@ -1,31 +1,29 @@
 #include <iostream>
 #include <vector>
+#define DEFAULT_N 100
 
 class DOMAIN1 {
     public:
         DOMAIN1(int argc, char** argv) {
             using namespace std;
-        // below... 
-        std::string nn = "-n";
-        if(argc < 2) 
-        {
-            N = 100;
-            cout<<"Inserted by default N = 100"<<endl;
-        }
-        else{
-            for(int i= 0; i < argc; i++)
-            {
-                if(nn.compare(argv[i]) == 0 && ( i+1 < argc ) )
+            // below... 
+            string nn = "-n";
+
+            if(argc < 2) cout<<"Inserted by default N = "<<DEFAULT_N<<endl;
+            else{
+                for(int i= 0; i < argc; i++)
                 {
-                    try{
-                        N = std::stoi(argv[i+1]);
-                    }catch(std::exception){
-                        cout<<"Please, insert a number after -n"<<endl;
+                    if(nn.compare(argv[i]) == 0 && ( i+1 < argc ) )
+                    {
+                        try{
+                            N = stoi(argv[i+1]);
+                        }catch(exception){
+                            cout<<"Please, insert a number after -n"<<endl<<"Inserted by default N = 100"<<endl;
+                        }
                     }
-                }
-                else if(nn.compare(argv[i]) == 0 && ( i+1 == argc )) cout<<"Please, insert a number after -n"<<endl;
-            }
-        } 
+                    else if(nn.compare(argv[i]) == 0 && ( i+1 == argc )) cout<<"Please, insert a number after -n"<<endl<<"Inserted by default N = 100"<<endl;
+                } 
+            } 
     }
 
     void printN(){
@@ -33,6 +31,6 @@ class DOMAIN1 {
     }
 
     private:
-        std::size_t N = 0;
+        std::size_t N = DEFAULT_N; // default value
         std::vector<int> Boundary = {0, 1000, 0, 1000};
 };
