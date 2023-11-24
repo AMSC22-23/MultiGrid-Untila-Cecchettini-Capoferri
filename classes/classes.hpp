c62405b34c75ec1f42ef2487d1e3062a9d250710
Denis Untila <denis.untila@mail.polimi.it>
2023-11-24 12:00:15 +0000

    test

--- classes/classes.hpp
+++ classes/classes.hpp
@@ -4,6 +4,24 @@
 #include <vector>
 
 
-namespace AMG
+namespace AMG{
+
+class SquareDomain{
+    public:
+        SquareDomain(const size_t &size, const double &length):m_size(size), m_length(length){
+            m_h = m_length / (m_size - 1);
+        }
+
+        std::tuple<double, double> coord(const size_t &i, const size_t &j){return {j* m_h, m_length - i * m_h};}
+
+        ~SquareDomain(){}
+
+    private:
+        size_t m_size;
+        double m_length;
+        double m_h;
+};
+
+}
 
 #endif
