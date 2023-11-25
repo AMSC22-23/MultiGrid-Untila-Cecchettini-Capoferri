32506d22ef445fee89352aaa24dfc7a4b608fb33
Denis Untila <denis.untila@mail.polimi.it>
2023-11-25 11:48:42 +0000

    hjbhjhb

--- classes/classes.hpp
+++ classes/classes.hpp
@@ -2,15 +2,18 @@
 #define CLASSES_H
 
 #include <tuple>
+#include <vector>
 
 namespace AMG{
 
 class Domain{
     public:
         virtual std::tuple<double,double> operator[](const size_t i) const = 0;
-        virtual bool isOnBoundary(size_t l) const = 0;
-        virtual size_t N() const = 0;
-        virtual double h() const = 0;
+        virtual const bool isOnBoundary(size_t l) const = 0;
+        virtual const size_t numBoundaryNodes() const = 0;
+        virtual const size_t numConnections() const = 0;
+        virtual const size_t N() const = 0;
+        virtual const double h() const = 0;
 };
 
 
@@ -28,14 +31,18 @@ class SquareDomain: public Domain{
             return coord(i,j); 
         }
 
-        bool isOnBoundary(size_t l) const override{
+        const bool isOnBoundary(size_t l) const override{
             size_t i = l / m_size;
             size_t j = l % m_size;
             return (((i == 0) || (j == 0) || (i == (m_size-1)) || (j == (m_size-1))) ? true : false);
         }
 
-        size_t N() const override{return m_size*m_size;}
-        double h() const override{return m_h;}
+        const size_t numBoundaryNodes() const override{return m_size * 4 - 4;}
+        const size_t numConnections() const override{
+            return (4 * (m_size * m_size - numBoundaryNodes()));
+        }
+        const size_t N() const override{return m_size*m_size;}
+        const double h() const override{return m_h;}
 
         ~SquareDomain(){}
 
@@ -45,12 +52,14 @@ class SquareDomain: public Domain{
         double m_h;
 };
 
+template<typename T>
 class PoissonMatrix{
     public:
         PoissonMatrix(Domain &domain):m_domain(domain), m_size(domain.N()) {}
 
-        const double coeffRef(const size_t i, const size_t j){
+        const T coeffRef(const size_t i, const size_t j){
             if(m_domain.isOnBoundary(i)){
+                //auto h = m_domain.h();
                 return ((j == i) ? 1. : 0.);
             }else{
                 if(j == i)
@@ -66,6 +75,10 @@ class PoissonMatrix{
                 else
                     return 0.;
             }
+        }
+
+        const size_t nonZeros(){
+            return m_size + m_domain.numConnections();
         }
 
         const size_t rows(){return m_size;}
--- classes/main.cpp
+++ classes/main.cpp
@@ -4,30 +4,33 @@
 #include "classes.hpp"
 
 
-
-int main(int argc, char** argv){
-    size_t size = 7;
-    AMG::SquareDomain dominio(size,10.);
-    AMG::PoissonMatrix A(dominio);
-
-    
-    int count = 0;
+template<class SpMat>
+void saveMatrixOnFile(SpMat A, std::string fileName){
 
     std::ofstream file;
-    file.open("out.mtx");
-    file<<A.rows()<<" "<<A.cols()<<std::endl;
+    file.open(fileName);
+    file<<A.rows()<<" "<<A.cols()<<" "<<A.nonZeros()<<std::endl;
 
     for(size_t i = 0; i < A.rows(); i++){
         for(size_t j = 0; j < A.cols(); j++){
             auto elem = A.coeffRef(i,j);
             if(elem != 0){
                 file<<i<<" "<<j<<" "<<elem<<std::endl;
-                count += 1;
             }
         }
     }
+    file.close();
+}
 
-    std::cout<<"Non zero entries = "<<count<<std::endl;
+
+int main(int argc, char** argv){
+    size_t size = std::atoi(argv[1]);
+    AMG::SquareDomain dominio(size,10.);
+    AMG::PoissonMatrix<double> A(dominio);
+    
+    saveMatrixOnFile<AMG::PoissonMatrix<double>>(A,"out.mtx");
+
+    std::cout<<"Non zero entries = "<<A.nonZeros()<<std::endl;
     
     return 0;
 }
