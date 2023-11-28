10b006b9d1c7fcaafa66a5ec7de03ad62154d388
Stefano Cecchettini <stefano.cecchettini1@gmail.com>
2023-11-28 11:29:26 +0100

    dev

--- include/domain.hpp
+++ include/domain.hpp
@@ -38,3 +38,52 @@ class DOMAIN {
         std::size_t N = DEFAULT_N; // default value
         std::vector<int> Boundary = {0, 1000, 0, 1000};
 };
+
+
+
+template <typename T> 
+class Jacobi1 : public System{
+
+    
+    public:
+
+        Jacobi1(unsigned int max_N_iterations, double tol) : Max_N_iterations(max_N_iterations), Tol(tol) {}
+
+        void Set_Tol(double tol){ Tol = tol; }
+        double Get_Tol(){ return Tol; }
+
+        void Set_Max_N_iterations(unsigned int max_N_iterations){ Max_N_iterations = max_N_iterations; }
+        double Get_Max_N_iterations(){ return Max_N_iterations; }
+
+
+        void iteration_method(std::vector<T>& sol) override{
+            
+            std::vector<T> x_sol;
+            unsigned int k = 0;
+            double sum;
+
+            while(k <= Max_N_iterations){
+                for(size_t i = 0; i < f.size(); i++){
+                    sum = 0;
+                    for(const auto &idx : A.nonZerosInRow(i)){
+                        if(idx != i){
+                            sum += A.coeffRef(i,idx) * x[idx];
+                        }
+                    }
+                    x_sol[i] = (f[i] - sum) / A.coeffRef(i,i);
+                }
+                if(std::abs() < Tol)
+                {
+                    sol = x_sol;
+                    break;
+                }
+                    
+                k++;
+            }
+            
+        }
+    
+    private:
+        unsigned int Max_N_iterations = 1000; 
+        double Tol; // tolerance
+}
