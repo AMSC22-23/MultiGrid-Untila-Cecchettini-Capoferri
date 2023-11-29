a5d04d397ae507481b670a30c2e1b9923dcf1360
Lucacapoferri <luca2.capoferri@mail.polimi.it>
2023-11-29 13:25:14 +0000

    correction

--- system.hpp
+++ system.hpp
@@ -1,77 +1,78 @@
-#include <iostream>
+ #include <iostream>
 #include "domain.hpp"
 #include <vector>
-#include <functions>
+//#include <functions>
 
 
 
 template<typename T,class T_VECT>
 class System{
     public:
-    System(AMG::PoissonMatrix<T>& Mat,T_VECT &F ) : mat(Mat), f(F){};
+    System(AMG::PoissonMatrix<T> &Mat,T_VECT &F ) : mat(Mat), f(F){};
     virtual void /*error*/ iteration_method(std::vector<T>& sol) const=0;
-    AMG::PoissonMatrix get_matrix_A (){return mat;}; 
+    AMG::PoissonMatrix<T> get_matrix_A (){return mat;}; 
     void multigrid(){/*to define*/};
     ~System(){};
 
     protected:
         T_VECT f;
-        AMG::PoissonMatrix mat;
+        AMG::PoissonMatrix<T> mat;
 };
 
 
 //gauss_saidel_method
-
-class GS : public System{
+template <typename T, class T_VECT>
+class GS : public System <T, T_VECT> {
 
 public:
-GS(int It, AMG::PoissonMatrix<T>& Mat,T_VECT &F) : iteration(It), mat(Mat), f(F) {};
-void iteration_method(std::vector<T>& sol) override{
+GS(int It, AMG::PoissonMatrix<T>& Mat,T_VECT &F) : System<T, T_VECT>(Mat,F), iteration(It), mat(Mat), f(F) {};
+void iteration_method(std::vector<T>& sol) const override{
     
-    std::vector<T> x_new(f.size(),0);
+    std::vector<T> X_new(f.size(),0);
     for(size_t k=0; k<iteration; k++){
     for(size_t i = 0; i<f.size(); i++){
         double sum1=0,sum2=0;
-        for(int id : A.nonZerosInRow(i)){
+        for(int id : mat.nonZerosInRow(i)){
             if(id < i)
-                sum1+= A.coeffRef(i,id)*X_new[id];
+                sum1+= mat.coeffRef(i,id)*X_new[id];
             else if(id > i)
-                sum2+= A.coeffRef(i,id)*sol[id];
+                sum2+= mat.coeffRef(i,id)*sol[id];
             
         }
-        X_new[i]= (f[i] - sum1 -sum2) / A.coeffRef(i,i);
+        X_new[i]= (f[i] - sum1 -sum2) / mat.coeffRef(i,i);
     }
      sol=X_new;
      X_new = X_new.assign(X_new.size(),0);
     }
-};
+}
 private:
 int iteration;
-}//chiedi gestione var..
+};
 
 
 //jacobi method 
-class Jacobi : public System{
+template <typename T, class T_VECT>
+class Jacobi : public System<T, T_VECT>{
 
 public:
-Jacobi(int It, AMG::PoissonMatrix<T>& Mat,T_VECT &F) : iteration(It), mat(Mat), f(F) {};
+Jacobi(int It, AMG::PoissonMatrix<T>& Mat,T_VECT &F) : System<T, T_VECT>(Mat,F), iteration(It), mat(Mat), f(F) {};
 void iteration_method(std::vector<T>& sol) override{
     
     std::vector<T> X_new(f.size(),0);
-    for(k=0; k<iteration;k++){
+    for(int k=0; k<iteration;k++){
     for(size_t i = 0; i < f.size(); i++){
         double sum = 0;
-        for(const auto &id : A.nonZerosInRow(i)){
+        for(const auto &id : mat.nonZerosInRow(i)){
             if(id != i){
-                sum += A.coeffRef(i,id) * sol[id];
+                sum += mat.coeffRef(i,id) * sol[id];
             }
         }
-        X_new[i] = (f[i] - sum) / A.coeffRef(i,i);
+        X_new[i] = (f[i] - sum) / mat.coeffRef(i,i);
     }
     sol = X_new;
     X_new = X_new.assign(X_new.size(),0);
     }
-};
+}
 private:
 int iteration;
-}
+};
